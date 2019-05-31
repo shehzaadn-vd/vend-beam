5022efd101454e3f813752f8d06ac9c3ece0d83e
Jawad Hyder <jawad@venturedive.com>
2019-05-31 11:58:06 +0500

    Updated JdbcUtils class to support ARRAY

--- sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcUtils.java
+++ sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcUtils.java
@@ -1,5 +1,6 @@
 package org.apache.beam.sdk.io.jdbc;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.beam.sdk.schemas.LogicalTypes;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.FieldType;
@@ -7,79 +8,79 @@ import org.apache.beam.sdk.schemas.Schema.Field;
 import org.apache.beam.sdk.values.Row;
 import org.joda.time.Instant;
 
-import java.sql.ResultSet;
+import java.sql.JDBCType;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.sql.ResultSet;
 
 public class JdbcUtils {
 
+    /*
+     * Mapping of JDBC types to Beam Schema types
+     * TODO: Add support for BLOB and CLOB types
+     */
+    private static final ImmutableMap<JDBCType, FieldType> JDBC_TO_BEAM_TYPE_MAPPING =
+            ImmutableMap.<JDBCType, FieldType>builder()
+                    .put(JDBCType.BIGINT, Schema.FieldType.INT64)
+                    .put(JDBCType.BINARY, Schema.FieldType.BYTES)
+                    .put(JDBCType.BIT, Schema.FieldType.BOOLEAN)
+                    .put(JDBCType.BOOLEAN, Schema.FieldType.BOOLEAN)
+                    .put(JDBCType.CHAR, Schema.FieldType.STRING)
+                    .put(JDBCType.DECIMAL, Schema.FieldType.DECIMAL)
+                    .put(JDBCType.DOUBLE, Schema.FieldType.DOUBLE)
+                    .put(JDBCType.FLOAT, Schema.FieldType.FLOAT)
+                    .put(JDBCType.INTEGER, Schema.FieldType.INT32)
+                    .put(JDBCType.LONGVARBINARY, Schema.FieldType.BYTES)
+                    .put(JDBCType.LONGVARCHAR, Schema.FieldType.STRING)
+                    .put(JDBCType.NUMERIC, Schema.FieldType.DECIMAL)
+                    .put(JDBCType.REAL, Schema.FieldType.FLOAT)
+                    .put(JDBCType.SMALLINT, Schema.FieldType.INT16)
+                    .put(JDBCType.TIMESTAMP, Schema.FieldType.DATETIME)
+                    .put(JDBCType.TINYINT, Schema.FieldType.INT16)
+                    .put(JDBCType.VARBINARY, Schema.FieldType.BYTES)
+                    .put(JDBCType.VARCHAR, Schema.FieldType.STRING)
+                    .build();
+
     /**
-     * Get the Beam {@link FieldType} from a {@link ResultSetMetaData ColumnType}.
+     * Get the Beam {@link FieldType} from a {@link java.sql.ResultSetMetaData ColumnType}.
      *
      * <p>Supports JDBC Types</p>
      *
-     * @param type of {@link ResultSetMetaData ColumnType}
+     * @param columnType of {@link java.sql.ResultSetMetaData ColumnType}
      * @return Corresponding Beam {@link FieldType}
      */
-    private static FieldType fromFieldType(int type) {
-        switch (type) {
-            case Types.VARCHAR:
-            case Types.CHAR:
-            case Types.LONGNVARCHAR:
-                return FieldType.STRING;
-            case Types.BIT:
-            case Types.BOOLEAN:
-                return FieldType.BOOLEAN;
-            case Types.NUMERIC:
-                return FieldType.DECIMAL;
-            case Types.TINYINT:
-                return FieldType.BYTE;
-            case Types.SMALLINT:
-                return FieldType.INT16;
-            case Types.INTEGER:
-                return FieldType.INT32;
-            case Types.BIGINT:
-                return FieldType.INT64;
-            case Types.FLOAT:
-                return FieldType.FLOAT;
-            case Types.REAL:
-            case Types.DOUBLE:
-                return FieldType.DOUBLE;
-            case Types.VARBINARY:
-            case Types.BINARY:
-            case Types.BLOB:
-                return FieldType.BYTES;
-            case Types.DATE:
-                return FieldType.logicalType(
+    private static FieldType toFieldType(int columnType, String columnTypeName) {
+        JDBCType jdbcType = JDBCType.valueOf(columnType);
+        switch (jdbcType) {
+            case ARRAY:
+                if (!JDBC_TO_BEAM_TYPE_MAPPING.containsKey(jdbcType))
+                    throw new UnsupportedOperationException("Array of type " + jdbcType.name() + " is not supported");
+                return JDBC_TO_BEAM_TYPE_MAPPING.get(jdbcType);
+            case DATE:
+                return Schema.FieldType.logicalType(
                         new LogicalTypes.PassThroughLogicalType<Instant>(
-                                "SqlDateType", "", FieldType.DATETIME) {});
-            case Types.TIME:
-                return FieldType.logicalType(
+                                "SqlDateType", "", Schema.FieldType.DATETIME) {});
+            case TIMESTAMP_WITH_TIMEZONE:
+                return Schema.FieldType.logicalType(
                         new LogicalTypes.PassThroughLogicalType<Instant>(
-                                "SqlTimeType", "", FieldType.DATETIME) {});
-            case Types.TIMESTAMP:
-                return FieldType.logicalType(
-                        new LogicalTypes.PassThroughLogicalType<Instant>(
-                                "SqlTimestampWithLocalTzType", "", FieldType.DATETIME) {});
+                                "SqlTimestampWithLocalTzType", "", Schema.FieldType.DATETIME) {});
             default:
-                throw new UnsupportedOperationException( "Conversion or sql type to Beam type is unsupported");
+                if (!JDBC_TO_BEAM_TYPE_MAPPING.containsKey(jdbcType)) {
+                    throw new UnsupportedOperationException("Representing fields of type [" + jdbcType.name() + "] in Beam schemas is not supported");
+                }
+                return JDBC_TO_BEAM_TYPE_MAPPING.get(jdbcType);
         }
     }
 
-    public static Schema fromResultSetMetaDataToSchema(ResultSetMetaData metaData) throws SQLException  {
+    public static Schema fromResultSetMetaDataToSchema(ResultSetMetaData metaData) throws SQLException {
         Schema.Builder schemaBuilder = Schema.builder();
         for (int i = 1; i <= metaData.getColumnCount(); i++) {
-            FieldType fieldType = fromFieldType(metaData.getColumnType(i));
-            Field field = Field.of(metaData.getColumnName(i), fieldType).withNullable(metaData.isNullable(i) != ResultSetMetaData.columnNoNulls);
+            FieldType fieldType = toFieldType(metaData.getColumnType(i), metaData.getColumnTypeName(i));
+            Field field = Field.of(metaData.getColumnLabel(i), fieldType).withNullable(metaData.isNullable(i) != ResultSetMetaData.columnNoNulls);
             schemaBuilder.addField(field);
         }
         return schemaBuilder.build();
-    }
-
-    /** Convert a JDBC {@link ResultSet} to a Beam {@link Schema}. */
-    public static Schema fromResultSetToSchema(ResultSet resultSet) throws SQLException {
-        return fromResultSetMetaDataToSchema(resultSet.getMetaData());
     }
 
     /**
